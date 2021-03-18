d18a03ae2010c6e01088770f5a7961c7b3e3cb5f
juan palomino <j.palomino92@yahoo.com.ar>
2021-03-18 10:28:30 +0100

    we create the index

---
+++ assets/JS/script.js
@@ -0,0 +1,9 @@
+alert("Bienvenido la calculadora que solamente suma.")
+let n1 = prompt("Ingrese un numero");
+let n2 = prompt("Ingrese otro numero");
+
+let resultado = parseInt(n1) + parseInt(n2);
+
+alert("el resultado es: " + resultado)
+
+
